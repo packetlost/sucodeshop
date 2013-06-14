d88df921e47931c536d0dae9148fbe01b8387b6c
sucodesoftware <socialmedia@sucode.net>
2013-06-14 21:11:52 +0200

    SimplecartJS integration

---
+++ sucodeshop/js/sucodeshop-config.js
@@ -0,0 +1,9 @@
+// ---------------------------
+// SUCODE SHOP CONFIGURATION
+// ---------------------------
+var paginateProducts = true;
+var productsPerPage = 9;
+var categoriesService = '';
+var productsService = '';
+var loadProductsOnDemand = true;
+
