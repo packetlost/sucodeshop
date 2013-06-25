9ebb893e4ec1fc5dc89c9e814f711c4f0e4e5b07
sucodesoftware <socialmedia@sucode.net>
2013-06-25 13:02:04 +0200

    Added dynamic loading of categories and products

--- sucodeshop/js/sucodeshop-config.js
+++ sucodeshop/js/sucodeshop-config.js
@@ -1,9 +1,31 @@
 // ---------------------------
 // SUCODE SHOP CONFIGURATION
 // ---------------------------
-var paginateProducts = true;
+
+// shopCurrency defines the currency used for e-shopping. See http://simplecartjs.org/documentation/currency  
+var shopCurrency = "EUR";
+// shopLanguage defines the language used in the shop.  
+var shopLanguage = "english-us";
+// Checkout options 
+var checkoutType = "SendForm"; 
+var checkoutUrl = "http://yourserver/your/custom/checkout/url";
+var checkoutMethod = "POST";
+
+//Define the services for retrieving the categories and products lists. 
+var categoriesService = '/shop/json/categories.json';
+// If the products service depends on the category id, use {id}. For example:'/shop/json/products/{id}.json','/shop/json/products/getProducts?categoryId={id}',...
+var productsService = '/shop/json/products/{id}.json';
+// Products images format
+var imgFormat = 'png';
+// Tax to apply to cart total (VAT)   
+taxValue = 0.21;
+
+// Set paginateProducts to true if there are many products for each category (for example, more than 10 products/category).
+var paginateProducts = false;
+// If pagination is enabled, determine the number of products per page. 
 var productsPerPage = 9;
-var categoriesService = '';
-var productsService = '';
-var loadProductsOnDemand = true;
+
+// vars used to store categories and products (referenced from the shop page). Not initialized by default. 
+var categories;
+var products;
 
